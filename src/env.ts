20c1c8ffd939306aafaf221136b63c7bf28cbfb9
Ryan Xu <xueyix@uw.edu>
2023-12-03 01:20:30 -0800

    1111

--- src/env.ts
+++ src/env.ts
@@ -3,11 +3,8 @@ export namespace Env {
     export const DEBUG: boolean = true;
     export const PORT: number = 5050;
     export const HOST: string = "localhost";
-<<<<<<< HEAD
     export const GoogleKey: string = '__GoogleKey__';
-=======
     export const GoogleKeyRyan: string = '__key__';
     export const OPENAI_API_KEY: string = '__key__'
     export let CHAT_INIT_PROMPT: string = "Hello JSON"
->>>>>>> e876c26b3bffb3b585cda17dffb91077c7e03662
 }
